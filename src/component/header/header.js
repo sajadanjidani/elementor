c73915b1b760bc712a97c87ecfde24497455d215
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-11 18:29:02 +0330

    finish coding header component

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -3,7 +3,25 @@ import styles from './header.module.css'
 const Header = () => {
     return(
         <div className={styles.container}>
-            <div className={styles.contentSide}></div>
+            <div className={styles.contentSide}>
+                <div className={styles.titleBox}>
+                    <h1 className={styles.title}>
+                        <span>افزونه المنتـــور پـــرو</span>
+                        <div className={styles.acsstoreBg}></div>    
+                    </h1>
+                    <p className={styles.subTitle}>بدون کد نویسی! باورت میشه ؟</p>
+                </div>
+                <p className={styles.subject}>ارائه اختصاصی افزونه المنتور پرو در راست چین، مختص کاربران ایرانی همراه با پکیج کامل ماژول های ایرانی</p>
+                <div className={styles.featuresBox}>
+                    <span>تقویم شمسی</span>
+                    <span>درون ریزی قالب ها</span>
+                    <span>فونت های فارسی</span>
+                </div>
+                <div className={styles.btnBox}>
+                    <button>خرید افزونه المنتور پرو</button>
+                    <button>تست انلاین صفحه ساز</button>
+                </div>
+            </div>
             <div className={styles.gifSide}>
                 <div className={styles.gifBox}>
                     <div>
