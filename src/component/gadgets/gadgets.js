b9e06c6f98248bd30ddd14ba18d200f1e040e7b0
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-14 23:20:43 +0330

    coding dynamic Gadgets component

--- src/component/gadgets/gadgets.js
+++ src/component/gadgets/gadgets.js
@@ -3,6 +3,21 @@ import GadgetsItem from '../gadgetsItem/gadgetsItem'
 
 const Gadgets = () => {
 
+    const itemInformation = [
+        {id: 1, title: 'تقویم شمسی', iconhref: 'goGo', bgIconColor: '#C22155', bgHover : '#FF8264'},
+        {id: 2, title: 'ویجت آپارات', iconhref: 'goGo', bgIconColor: '#C22155', bgHover : '#FF8264'},
+        {id: 3, title: 'ویجت ایرانی', iconhref: 'goGo', bgIconColor: '#4E2465', bgHover : '#7D9CE9'},
+        {id: 4, title: 'فونت فارسی', iconhref: 'goGo', bgIconColor: '#C22155', bgHover : '#FF8264'},
+        {id: 5, title: 'کد کوتاه', iconhref: 'goGo', bgIconColor: '#C22155', bgHover : '#FF8264'},
+        {id: 6, title: 'اسلایدر ها', iconhref: 'goGo', bgIconColor: '#4E2465', bgHover : '#7D9CE9'},
+        {id: 7, title: 'ویجت کاروسل', iconhref: 'goGo', bgIconColor: '#C22155', bgHover : '#FF8264'},
+        {id: 8, title: 'ویجت فرم', iconhref: 'goGo', bgIconColor: '#4E2465', bgHover : '#7D9CE9'},
+        {id: 9, title: 'گردونه', iconhref: 'goGo', bgIconColor: '#4E2465', bgHover : '#7D9CE9'},
+        {id: 10, title: 'جست و جو', iconhref: 'goGo', bgIconColor: '#C22155', bgHover : '#FF8264'},
+        {id: 11, title: 'لیست قیمت', iconhref: 'goGo', bgIconColor: '#4E2465', bgHover : '#7D9CE9'},
+        {id: 12, title: 'تایمر معکوس', iconhref: 'goGo', bgIconColor: '#4E2465', bgHover : '#7D9CE9'}
+    ]
+
     return(
         <div className={styles.container}>
             <div className={styles.titleBox}>
@@ -11,18 +26,18 @@ const Gadgets = () => {
                 <p className={styles.subTitle}>برخی از ویجت ها قابلیت های پرکاربرد:</p>
             </div>
             <div className={styles.ItemContainer}>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
-                <GadgetsItem></GadgetsItem>
+                <GadgetsItem {...itemInformation[0]} />
+                <GadgetsItem {...itemInformation[1]} />
+                <GadgetsItem {...itemInformation[2]} />
+                <GadgetsItem {...itemInformation[3]} />
+                <GadgetsItem {...itemInformation[4]} />
+                <GadgetsItem {...itemInformation[5]} />
+                <GadgetsItem {...itemInformation[6]} />
+                <GadgetsItem {...itemInformation[7]} />
+                <GadgetsItem {...itemInformation[8]} />
+                <GadgetsItem {...itemInformation[9]} />
+                <GadgetsItem {...itemInformation[10]} />
+                <GadgetsItem {...itemInformation[11]} />
             </div>
         </div>
     )
--- src/component/gadgetsItem/gadgetsItem.js
+++ src/component/gadgetsItem/gadgetsItem.js
@@ -1,10 +1,26 @@
 import styles from './gadgetsItem.module.css'
 
-const GadgetsItem = () => {
+const GadgetsItem = ({title, bgIconColor, bgHover}) => {
+
+    
+    const handleMouseEnter = e => {
+        let target = e.target;
+        target.style.backgroundColor = bgHover
+    }
+
+    const handleMouseLeave = e => {
+        let target = e.target;
+        target.style.backgroundColor = bgIconColor
+    }    
+
+    const iconContainerStyle = {
+        backgroundColor: bgIconColor,
+    }
+
     return(
         <div className={styles.itemContainer}>
-            <div className={styles.iconContainer}></div>
-            <h1 className={styles.itemName}>تقویم شمسی</h1>
+            <div className={styles.iconContainer} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={iconContainerStyle}></div>
+            <h1 className={styles.itemName}>{title}</h1>
         </div>
     )
 } 
