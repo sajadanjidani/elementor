f7f733e484b971af5c087769504066307bd269af
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-15 16:09:17 +0330

    finish responsive coding Header component

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,34 +1,35 @@
 import styles from './header.module.css'
+import medias from './headerMedia.module.css'
 
 const Header = () => {
     return(
-        <div className={styles.container}>
+        <div id={medias.container} className={styles.container}>
             <div className={styles.contentSide}>
                 <div className={styles.titleBox}>
-                    <h1 className={styles.title}>
+                    <h1 id={medias.title} className={styles.title}>
                         <span>افزونه المنتـــور پـــرو</span>
                         <div className={styles.acsstoreBg}></div>    
                     </h1>
                     <p className={styles.subTitle}>بدون کد نویسی! باورت میشه ؟</p>
                 </div>
-                <p className={styles.subject}>ارائه اختصاصی افزونه المنتور پرو در راست چین، مختص کاربران ایرانی همراه با پکیج کامل ماژول های ایرانی</p>
-                <div className={styles.featuresBox}>
+                <p id={medias.subject} className={styles.subject}>ارائه اختصاصی افزونه المنتور پرو در راست چین، مختص کاربران ایرانی همراه با پکیج کامل ماژول های ایرانی</p>
+                <div id={medias.featuresBox} className={styles.featuresBox}>
                     <span>تقویم شمسی</span>
                     <span>درون ریزی قالب ها</span>
                     <span>فونت های فارسی</span>
                 </div>
-                <div className={styles.btnBox}>
+                <div id={medias.btnBox} className={styles.btnBox}>
                     <button>خرید افزونه المنتور پرو</button>
                     <button>تست انلاین صفحه ساز</button>
                 </div>
             </div>
             <div className={styles.gifSide}>
-                <div className={styles.gifBox}>
+                <div id={medias.gifBox} className={styles.gifBox}>
                     <div>
-                        <span>با المنتور سایتتو سریعتر بساز</span>
-                        <div className={styles.cercle}></div>
+                        <span id={medias.titleGifBox}>با المنتور سایتتو سریعتر بساز</span>
+                        <div id={medias.cercle} className={styles.cercle}></div>
                     </div>
-                    <h2>سایت حرفه ای، امکانات حرفه ای، المنتور حرفه ای</h2>
+                    <h2 id={medias.subTitleGifBox}>سایت حرفه ای، امکانات حرفه ای، المنتور حرفه ای</h2>
                 </div>
             </div>
         </div>
