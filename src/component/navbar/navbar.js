6aac634a976b0858cd6dbe9a7dc9bc1c45c864b3
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-15 15:22:29 +0330

    responsive coding Navbar component

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -17,42 +17,47 @@ const Navbar = () => {
         }
     };
     return(
-        <div className={styles.container}>
-            <div id={medias.hamberMenu} className={styles.hamberMenu}></div>
-            <div className={styles.imagebox}>
-                <img className={styles.imageElementPro} src='/image/navbar/elementor-pro-logo.png' alt='elementProImage' />
+        <div>
+            <div id={medias.imageSm} className={styles.imageSm}>
+                <img className={styles.imageElementPro} src='/image/navbar/elementor-pro-logo.png' alt='elementProImage' /> 
             </div>
-            <div id={medias.menuList}>
-                <ul className={styles.menuList}>
-                    <li>
-                        <a href='#'> سوالات متداول </a>
-                        </li>
-                    <li>
-                        <a href='#'> ابزارک ها </a>
-                        </li>
-                    <li id={styles.hasMenu} onMouseEnter={() => mouseHover()} onMouseLeave={() => mouseUnHover()}>
-                        <a href='#'> ویژگی ها </a>
-                        {/* has menu */}
-                        <div id={styles.subMenu} ref={subMenuRef}>
-                            <p>ایکون</p>
-                            <p>قالب ها</p>
-                        </div>
-                        </li>
-                    <li>
-                        <a href='#'> المنتور راست چین </a>
-                        </li>
-                    <li>
-                        <a href='#'> پشتیبانی </a>
-                        </li>
-                    <li>
-                        <a href='#'> تست افزونه </a>
-                        </li>
-                </ul>
-            </div>
-            <div>
-                <button className={styles.btnBuy}>
-                    <span>خرید افزونه</span>
-                </button>
+            <div id={medias.container} className={styles.container}>
+                <div id={medias.hamberMenu} className={styles.hamberMenu}></div>
+                <div id={medias.imagebox} className={styles.imagebox}>
+                    <img className={styles.imageElementPro} src='/image/navbar/elementor-pro-logo.png' alt='elementProImage' />
+                </div>
+                <div id={medias.menuList}>
+                    <ul className={styles.menuList}>
+                        <li>
+                            <a href='#'> سوالات متداول </a>
+                            </li>
+                        <li>
+                            <a href='#'> ابزارک ها </a>
+                            </li>
+                        <li id={styles.hasMenu} onMouseEnter={() => mouseHover()} onMouseLeave={() => mouseUnHover()}>
+                            <a href='#'> ویژگی ها </a>
+                            {/* has menu */}
+                            <div id={styles.subMenu} ref={subMenuRef}>
+                                <p>ایکون</p>
+                                <p>قالب ها</p>
+                            </div>
+                            </li>
+                        <li>
+                            <a href='#'> المنتور راست چین </a>
+                            </li>
+                        <li>
+                            <a href='#'> پشتیبانی </a>
+                            </li>
+                        <li>
+                            <a href='#'> تست افزونه </a>
+                            </li>
+                    </ul>
+                </div>
+                <div>
+                    <button className={styles.btnBuy}>
+                        <span>خرید افزونه</span>
+                    </button>
+                </div>
             </div>
         </div>
     )
