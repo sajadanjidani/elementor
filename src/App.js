c7c141f0b93003e1f98a576748f539445ad695ab
sajadanjidani <sajadanjiidanii@gmail.com>
2025-04-16 16:47:11 +0330

    coding dynamic Section Item component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import SectionItem from './component/sectionItem/sectionItem';
 import ItemDescrip from './component/itemDescrip/itemDescrip';
 
 function App() {
+
+  const sectionItemInfo = [
+    {id: 1, title: 'بیش از 100 ابزارک اختصاصی', subTitle: 'المنتور پرو به شما امکان دسترسی به طیف گسترده‌ای از ابزارک‌های پیشرفته و تخصصی را می‌دهد که در سایر صفحه سازها کمتر یافت می‌شود.', bgIconColor: '#C22155', bgHover: '#FF8264'},
+    {id: 2, title: 'سرعت بخشیدن به طراحی', subTitle: 'با استفاده از قالب‌های از پیش طراحی شده، دیگر نیازی به ساخت هر بخش از سایت از ابتدا نیست.', bgIconColor: '#C22155', bgHover: '#FF8264'},
+    {id: 3, title: '', subTitle: 'انتخاب یک پوسته‌ساز حرفه‌ای می‌تواند به شما در ایجاد وب‌سایت‌های زیبا، کارآمد و حرفه‌ای کمک کند. با توجه به ویژگی‌های ذکر شده، شما می‌توانید پوسته‌ساز مناسب برای پروژه خود را انتخاب کنید.', bgIconColor: '#C22155', bgHover: '#FF8264'},
+    {id: 4, title: '', subTitle: 'برخی از پوسته‌سازهای حرفه‌ای به شما اجازه می‌دهند تا قالب‌های سفارشی خود را ایجاد کرده و آن‌ها را ذخیره کنید. این ویژگی به شما امکان می‌دهد تا یک سبک بصری یکپارچه در کل وب‌سایت خود ایجاد کرده و سرعت طراحی را افزایش دهید.', bgIconColor: '#4E2465', bgHover: '#7D9CE9'},
+    {id: 5, title: '', subTitle: 'می‌توان قالب‌های متفاوتی را برای دستگاه‌های مختلف (موبایل، تبلت، دسکتاپ) ایجاد کرد. مثال: نمایش یک هدر متفاوت برای کاربران موبایل نسبت به نسخه دسکتاپ.', bgIconColor: '#C22155', bgHover: '#FF8264'},
+    {id: 6, title: '', subTitle: 'امکان تنظیم اینکه هدر، فوتر، یا بخش‌های دیگر فقط در صفحات خاصی نمایش داده شوند. مثال: می‌توانید یک هدر سفارشی فقط برای صفحه فروشگاه یا یک فوتر اختصاصی فقط برای وبلاگ ایجاد کنید.', bgIconColor: '#4E2465', bgHover: '#7D9CE9'}
+  ]
+
   return (
     <div className='orginContainer'>
       <Navbar></Navbar>
@@ -15,18 +25,18 @@ function App() {
       <Gadgets></Gadgets>
       <SectionBannerOne></SectionBannerOne>
       <AboutSection>
-        <SectionItem />
-        <SectionItem />
+        <SectionItem {...sectionItemInfo[0]} />
+        <SectionItem {...sectionItemInfo[1]} />
         <ItemDescrip />
       </AboutSection>
       <AboutSection>
         <ItemDescrip />
-        <SectionItem />
-        <SectionItem />
+        <SectionItem {...sectionItemInfo[2]} />
+        <SectionItem {...sectionItemInfo[3]} />
       </AboutSection>
       <AboutSection>
-        <SectionItem />
-        <SectionItem />
+        <SectionItem {...sectionItemInfo[4]} />
+        <SectionItem {...sectionItemInfo[5]} />
         <ItemDescrip />
       </AboutSection>
     </div>
--- src/component/sectionItem/sectionItem.js
+++ src/component/sectionItem/sectionItem.js
@@ -1,11 +1,26 @@
 import styles from './sectionItem.module.css'
 
-const SectionItem = () => {
+const SectionItem = ({title, subTitle, bgIconColor, bgHover}) => {
+
+    const handleMouseEnter = e => {
+        let target = e.target;
+        target.style.backgroundColor = bgHover
+    }
+
+    const handleMouseLeave = e => {
+        let target = e.target;
+        target.style.backgroundColor = bgIconColor
+    }    
+
+    const iconContainerStyle = {
+        backgroundColor: bgIconColor,
+    }
+
     return(
         <div className={styles.container}>
-            <div className={styles.iconContainer}></div>
-            <h1 className={styles.title}>سرعت بخشیدن به طراحی</h1>
-            <p className={styles.subTitle}>با استفاده از قالب‌های از پیش طراحی شده، دیگر نیازی به ساخت هر بخش از سایت از ابتدا نیست.</p>
+            <div className={styles.iconContainer} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  style={iconContainerStyle}></div>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.subTitle}>{subTitle}</p>
         </div>
     )
 }
